204d11765512485e4331092f47117d5c3eb44312
akam alam <arundelfootball23@gmail.com>
2020-09-22 01:06:26 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -8,7 +9,6 @@ end
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-
 def engage
   puts state_log(date)
   date = generate_star_date
